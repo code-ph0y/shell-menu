d29ca6984d42abcfd05bd4234943c869fb688f73
Ross Moroney <code.ph0y@googlemail.com>
2017-06-25 20:46:11 +0100

    Comment: Menu.sh

--- menu.sh
+++ menu.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
 done
 
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Get the project functions
 source "$DIR/functions.sh";
+
+# Get main header - change menu header in config
 get_menu_header;
+# Get menu options
 get_menu_options;
+# Request input from user
 take_in_input;
 
+# Deal with sub menus
 while true
 do
 	get_menu_header;
