5a94f6c13fac81f255ef3ab890ba06bfc5c72068
Ross Moroney <code.ph0y@googlemail.com>
2025-07-15 17:18:12 +0100

    Fix color issues with cross platform

--- lib/ui.sh
+++ lib/ui.sh
@@ -2,19 +2,16 @@
 
 if [ "$USE_COLOR" = true ]; then
     # ANSI color codes
-    RESET="\033[0m"
-    BOLD="\033[1m"
+    RESET=$(printf '\033[0m')
+    BOLD=$(printf '\033[1m')
 
     # Foreground colors
-    FG_RED="\033[31m"
-    FG_GREEN="\033[32m"
-    FG_YELLOW="\033[33m"
-    FG_BLUE="\033[34m"
-    FG_CYAN="\033[36m"
-    FG_WHITE="\033[97m"
-
-    # Background colors
-    BG_BLUE="\033[44m"
+    FG_RED=$(printf '\033[31m')
+    FG_GREEN=$(printf '\033[32m')
+    FG_YELLOW=$(printf '\033[33m')
+    FG_BLUE=$(printf '\033[34m')
+    FG_CYAN=$(printf '\033[36m')
+    FG_WHITE=$(printf '\033[97m')
 else
     # No color
     RESET=""
